c6e8f1cb3ec9cb3f55d93c91f582f4e2f22ffba4
marilena <marilenalazari@gmail.com>
2020-12-15 18:07:25 +0200

    Intro
    dashboard θα γίνει login

---
+++ Part1_TransferTik/app/src/main/java/com/example/part1_transfertik/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.part1_transfertik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //Variables for starting animation
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        //1st Page with welcome Message
+        //Animations
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        //Hooks
+        image=findViewById(R.id.imageView);
+        text=findViewById(R.id.textView);
+
+        image.setAnimation(topAnim);
+        text.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
